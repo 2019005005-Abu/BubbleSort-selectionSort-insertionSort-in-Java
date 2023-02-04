5c16e93cb29c96136cb313b4bc7aa83f16c8db6d
Abu Al Shahriar Rifat <rifatdiu2686@gmail.com>
2023-02-04 12:24:14 +0600

    Buble sort &  Insertion sort

---
+++ index.java
@@ -0,0 +1,53 @@
+class Sorting_Alogorithoms{
+  public static void BubleSorting(int arr[]) {
+    for(int i=0;i<arr.length;i++){
+       System.out.println(arr[i]);
+    }
+    System.out.println();
+  }
+  public static void insertioSort(int arr[]) {
+    for(int i=0;i<arr.length;i++){
+       System.out.println(arr[i]);
+    }
+    System.out.println();
+  }
+
+
+  public static void main(String[] args) {
+    int arr[]={7,8,3,1,2};
+    //buble
+     for(int i=0;i<arr.length-1;i++){
+          for(int j=0;j<arr.length-i-1;j++){
+             if(arr[j]>arr[j+1]){
+               int temp=0;
+               temp=arr[j];
+               arr[j]=arr[j+1];
+               arr[j+1]=temp;
+             }
+          }
+     }
+     System.out.println("Buble Sort");
+     BubleSorting(arr);
+      
+
+     //selection sort
+     for(int i=0;i<arr.length-1;i++){
+      int smallest=i;
+      for(int j=i+1;j<arr.length;j++){
+        if(arr[smallest]>arr[j]){
+          smallest=j;
+        }
+      }
+      int temp=0;
+      temp=arr[smallest];
+      arr[smallest]=arr[i];
+      arr[i]=temp;
+     }
+     System.out.println("Insertion sort");
+     insertioSort(arr);
+     
+     //insertion sort
+     
+     
+  }
+}
